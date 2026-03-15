3099416a530089ec30a77aa01995912e8deda568
AlberVicky09 <albervicky09@gmail.com>
2026-03-15 10:45:27 +0100

    Add current gathered resource icon

--- UnityProject/Assets/Scripts/Controller/ActionUIController_v2.cs
+++ UnityProject/Assets/Scripts/Controller/ActionUIController_v2.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class ActionUIController_v2 : MonoBehaviour {
 
-    public RectTransform actionProgress, currentAction, canvasRect;
+    public RectTransform actionProgress, currentAction, currentActionAux, canvasRect;
     public Canvas actionCanvas;
     public Transform currentGameObject;
-    public Image actionProgressImage, currentActionImage;
+    public Image actionProgressImage, currentActionImage, currentActionAuxImage;
 
     public float iconWorldOffset = 2f;
+    public float auxIconWorldOffset = 2f;
     
     protected float progressTime, totalProgressTime;
-    private float orthoRatio, heightOffset, widthOffset;
     
     private Vector3 screenPoint;
-    private Vector2 localPoint;
+    private Vector2 localPoint, auxLocalPoint;
 
     private void LateUpdate() {
         //Calculate zoom ratio (0 when farthest, 1 when closest)
@@ -45,6 +47,16 @@ public class ActionUIController_v2 : MonoBehaviour {
             progressTime += Time.deltaTime;
 
             actionProgress.localPosition = localPoint;
+            if (currentActionAux != null) {
+                var worldPosAux = currentGameObject.position + Vector3.up + Vector3.right * auxIconWorldOffset;
+                var screenPointAux = CameraMove.Instance.cameraGO.WorldToScreenPoint(worldPosAux);
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    canvasRect,
+                    screenPointAux,
+                    actionCanvas.worldCamera,
+                    out auxLocalPoint);
+                currentActionAux.localPosition = auxLocalPoint;
+            }
         }
     }
     
@@ -52,12 +64,19 @@ public class ActionUIController_v2 : MonoBehaviour {
         actionProgress.gameObject.SetActive(false);
         currentAction.gameObject.SetActive(true);
         currentActionImage.sprite = displayImage;
+        if (currentActionAux != null) {
+            currentActionAuxImage.sprite = displayImage;
+            currentActionAux.gameObject.SetActive(false);
+        }
         LayoutRebuilder.ForceRebuildLayoutImmediate(canvasRect);
     }
 
     public void DisplayProgress() {
         actionProgress.gameObject.SetActive(true);
         currentAction.gameObject.SetActive(false);
+        if (currentActionAux != null) {
+            currentActionAux.gameObject.SetActive(true);
+        }
         progressTime = 0f;
     }
 }
